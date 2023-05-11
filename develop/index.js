6f1521b781dd0bd8ca50b4ddd1a58413fa89a3b9
Alvaro Vazquez <alvarovazquez@MacBook-Air.local>
2023-05-10 23:04:00 -0600

    added time and day variables

--- develop/index.js
+++ develop/index.js
@@ -1,5 +1,14 @@
-var date = dayjs().format('dddd, MMMM YYYY');
-var dateTime = dayjs().format('HH:MM:ss A')
+let key = 'cef775ad585cc9e589a2e29c31974382';
 
-console.log(dateTime)
+
+let date = dayjs().format('dddd, MMMM YYYY');
+let time = dayjs().format('HH:MM:ss A')
+
+console.log(time)
 console.log(date)
+
+var todayBodyCard = $('.todayBodyCard')
+
+function getWeather(){
+
+}
