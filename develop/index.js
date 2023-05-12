77030686e80833da30259498291b53afd44d2cc7
Alvaro Vazquez <alvarovazquez@MacBook-Air.local>
2023-05-12 02:39:50 -0600

    function for five day forecast

--- develop/index.js
+++ develop/index.js
@@ -5,9 +5,9 @@ let cities = [];
 
 function currentDay(date){
     var date = new Date();
-    let month = date.getMonth()+1;
-    let day = date.getDate();
-    let dayFormat = date.getFullYear() + '/' +
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    var dayFormat = date.getFullYear() + '/' +
         (month<10 ? '0' : '') + month + '/' +
         (day<10 ? '0' : '') + day;
     return dayFormat;
@@ -96,9 +96,57 @@ function getResponseWeather(cityName){
     var CoordLongitude = response.coord.lon;
     var CoordLatitude = response.coord.lat; 
 	var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + key;
-           
-      
+	
+		
+	$.ajax({
+		url: queryURL3,
+		method: "GET"
+	}).then(function(response5day) { 
+		$("#boxes").empty();
+		console.log(response5day);
+		for(var i=0, t=0; t<=6; i=i+6){
+			var read_date = response5day.list[i].dt;
+			if(response5day.list[i].dt != response5day.list[i+1].dt){
+				var FivedayDiv = $("<div>");
+				FivedayDiv.attr("class","col-3 m-2 bg-primary")
+				var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+				d.setUTCSeconds(read_date);
+				var date = d;
+				console.log(date);
+				var month = date.getMonth()+1;
+				var day = date.getDate();
+				var dayOutput = date.getFullYear() + '/' +
+				(month<10 ? '0' : '') + month + '/' +
+				(day<10 ? '0' : '') + day;
+				var Fivedayh4 = $("<h6>").text(dayOutput);
+				//Set src to the imags
+				var imgtag = $("<img>");
+				var skyconditions = response5day.list[i].weather[0].main;
+				if(skyconditions==="Clear"){
+					imgtag.attr("src",  "https://openweathermap.org/img/wn/01d@2x.png")
+				} else if(skyconditions==="Clouds"){
+					imgtag.attr("src","https://openweathermap.org/img/wn/03d@2x.png")
+				}else if(skyconditions==="Rain"){
+					imgtag.attr("src", "https://openweathermap.org/img/wn/10d@2x.png")
+				}
 
-    });
+				
+				
+				var temperature= response5day.list[i].main.temp;
+				console.log(skyconditions);
+				var tempNum = parseInt((temperature)* 9/5 - 459);
+				var fTemp = $("<p>").text("Tempeture: "+ tempNum + " °F");
+				var humPer = $("<p>").text("Humidity: "+ response5day.list[i].main.humidity + " %");
+				FivedayDiv.append(Fivedayh4);
+				FivedayDiv.append(imgtag);
+				FivedayDiv.append(fTemp);
+				FivedayDiv.append(humPer);
+				$("#boxes").append(FivedayDiv);
+				console.log(response5day);
+				t++;
+			}
+		}
+});
+	 });
     
   }
